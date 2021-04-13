071a6c52d3868d7a69557dc06b2e25213f9e4c41
Alvin Yap <alvin.yap@gmail.com>
2021-04-13 18:34:17 -0400

    AY - init commit

---
+++ sct-finance-service/src/main/java/com/sct/finance/SctFinancePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.sct.finance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SctFinancePlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SctFinancePlatformApplication.class, args);
+	}
+
+}
